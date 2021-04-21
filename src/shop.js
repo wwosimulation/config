99c716d34fec14248b0d894b7e5e6a8befb2208c
Shadow <william.shadow@outlook.com>
2021-04-21 15:23:34 -0500

    add roles

--- src/shop.js
+++ src/shop.js
@@ -11,7 +11,7 @@ module.exports = {} // This initalizes the shop
 /*
 
 Format:
-{ id: "uniqueid", name: "Item Name", price: 100, currency: "rose", description: "Description Here", role: 102937912392173597},
+{ id: "uniqueid", name: "Item Name", price: 100, currency: "rose", description: "Description Here", role: "102937912392173597"},
 
 The ID is what will be checked when doing +buy.
 Generally this should be the first word of the name, in all lowercase
@@ -21,16 +21,16 @@ The role is the role given when the item is bought, delete that part if it doesn
 */
 module.exports.items = [
   { id: "color", name: "Color Role", price: 50, currency: "coin", description: "See all the available colors by using `+shop colors`" },
-  { id: "dj", name: "DJ Role", price: 450, currency: "coin", description: "Gain the ability to use the music bots both here and in a game!" },
-  { id: "immunity", name: "Immunity", price: 10, currency: "rose", description: "When spectating, I will not change your name to lazy spectator" },
+  { id: "dj", name: "DJ Role", price: 450, currency: "coin", description: "Gain the ability to use the music bots both here and in a game!", role: "606123674562723840" },
+  { id: "immunity", name: "Immunity", price: 10, currency: "rose", description: "When spectating, I will not change your name to lazy spectator", role: "691390867822477413" },
   { id: "profile", name: "Profile Command", price: 200, currency: "coin", description: "Gain the ability to use +profile" },
-  { id: "ranked", name: "Ranked Pass", price: 1500, currency: "coin", description: "Gain access to play ranked games!" },
-  { id: "emoji", name: "Emoji", price: 450, currency: "coin", description: "Gain access to the `-write` command" },
+  { id: "ranked", name: "Ranked Pass", price: 1500, currency: "coin", description: "Gain access to play ranked games!", role: "832845602009645116" },
+  { id: "emoji", name: "Emoji", price: 450, currency: "coin", description: "Gain access to the `-write` command", role: "663389088354664477" },
   { id: "rose", name: "Rose", price: 25, currency: "coin", description: "Buy these to send someone a rose" },
   { id: "bouquet", name: "Rose Bouquet", price: 250, currency: "coin", description: "Use these to give everyone in the game a rose, including yourself!" },
-  { id: "grey", name: "Grey Color Role", price: 200, currency: "coin", description: "Gain the very rare Grey Color role" },
+  { id: "grey", name: "Grey Color Role", price: 200, currency: "coin", description: "Gain the very rare Grey Color role", role: "606123657210757136" },
   { id: "cmi", name: "Custom Maker Item", price: 1500, currency: "coin", description: "You can use this to create your own role list when playing.\nThis does not give you the full set of role list. You need to buy the rest of the roles using coins or roses. To check the list, do `+cmi`!" },
-  { id: "channel", name: "Private Channel", price: 100, currency: "rose", description: "Create your own private channel in this server!" },
+  { id: "channel", name: "Private Channel", price: 100, currency: "rose", description: "Create your own private channel in this server!", role: "627539599862005760" },
   { id: "icon", name: "Icon", price: 20, currency: "rose", description: "Add an icon to your profile!" },
   { id: "description", name: "Description", price: 25, currency: "rose", description: "Customize your profile description!" },
 ]
