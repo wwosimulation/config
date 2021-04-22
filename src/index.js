d3a2f56bbb7d7b699ed0641c487c9887c146a31c
Shadow <william.shadow@outlook.com>
2021-04-22 18:46:57 +0000

    add emojis

--- src/index.js
+++ src/index.js
@@ -26,3 +26,9 @@ module.exports = {
 module.exports.bombPlacements = ["1 2 3", "2 3 4", "5 6 7", "6 7 8", "9 10 11", "10 11 12", "13 14 15", "14 15 16", "1 5 9", "5 9 13", "2 6 10", "6 10 14", "3 7 11", "7 11 15", "4 8 12", "8 12 16", "1 6 11", "2 7 12", "3 6 9", "4 7 10", "5 10 15", "6 11 16", "7 10 13", "8 11 14"]
 module.exports.ids = require("./ids.js")
 module.exports.shop = require("./shop.js")
+module.exports.emojis = {
+  coin: `<:coin:606434686931173377>`,
+  rose: `<:rosesingle:807256844191793158>`,
+  gem: `<:gem:833431075309092904>`,
+  bouquet: `<:rosebouquet:808545517209387008>`,
+}
