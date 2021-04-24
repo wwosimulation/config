74d6f57de5d7728926cd82bf0c076d1aa0a6f2a6
Shadow <william.shadow@outlook.com>
2021-04-24 03:11:00 +0000

    xp config

---
+++ src/xp.js
@@ -0,0 +1,29 @@
+/*
+
+This file holds all the values for XP given after a game
+
+Format:
+
+"id": number,
+
+The ID is given by Shadow
+
+*/
+
+// XP values for non-teams
+module.exports = {
+  finishGame: 15,
+  winStreak: 25,
+  firstWinOfTheDay: 100,
+}
+
+// XP Values for each team
+module.exports.team = {
+  couple: 95,
+  werewolf: 75,
+  village: 55,
+  fool: 150,
+  headhunter: 150,
+  solo: 175,
+  tie: 15,
+}
