fb63029b75bc3fa7e0f05357621f071af47f11ef
Shadow <william.shadow@outlook.com>
2021-04-26 15:27:00 +0000

    cmi start

---
+++ src/cmi.js
@@ -0,0 +1,32 @@
+/*
+
+This file holds all the information for the cmi shop.
+
+Format:
+{ name: "Role Name", price: 100, currency: "rose"},
+
+The role name must be exactly how it is in +roleinfo
+The currency can be either "gem", "rose", or "coin"
+
+*/
+module.exports = [
+  { name: "Mayor", price: 75, currency: "coin" },
+  { name: "Grumpy Grandma", price: 75, currency: "coin" },
+  { name: "Seer Apprentice", price: 75, currency: "coin" },
+  { name: "Tough Guy", price: 75, currency: "coin" },
+  { name: "Loudmouth", price: 75, currency: "coin" },
+  { name: "Sorcerer", price: 75, currency: "coin" },
+  { name: "Flower Child", price: 75, currency: "coin" },
+  { name: "Guardian Wolf", price: 75, currency: "coin" },
+]
+
+// let allroles = ["Alchemist", "Grave Robber", "Fortune Teller", "Kitten Wolf", "Pacifist", "Spirit Seer", "Sheriff", "Werewolf Berserk", "Wolf Pacifist", "Cupid", "President", "Mayor", "Grumpy Grandma", "Seer Apprentice", "Tough Guy", "Loudmouth", "Sorcerer", "Flower Child", "Guardian Wolf", "Beast Hunter", "Avenger", "Witch", "Detective", "Forger", "Cursed", "Marksman", "Red Lady", "Junior Werewolf", "Nightmare Werewolf", "Shadow Wolf", "Random Regular Villager", "Random Strong Villager", "Random Werewolf", "Random Killer", "Random Voting", "Random", "Arsonist", "Sect Leader", "Bomber", "Zombie", "Corruptor", "Cannibal", "Illusionist", "Bandit"]
+
+// let msg = ""
+// let boughtroles = db.get(`boughtroles_${message.author.id}`) || []
+
+// let roses25 = ["Grave Robber", "Fortune Teller", "Pacifist", "Spirit Seer", "Sheriff", "Werewolf Berserk", "Kitten Wolf"]
+// let roses50 = ["Cupid", "President"]
+
+// let coins250 = ["Beast Hunter", "Avenger", "Witch", "Detective", "Forger", "Cursed", "Marksman", "Red Lady", "Junior Werewolf", "Nightmare Werewolf", "Shadow Wolf", "Random Regular Villager", "Random Strong Villager", "Random Werewolf", "Random Killer", "Random Voting", "Random"]
+// let coins1000 = ["Arsonist", "Sect Leader", "Bomber", "Zombie", "Corruptor", "Cannibal", "Illusionist", "Bandit", "Alchemist"]
