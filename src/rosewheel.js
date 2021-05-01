3985a9264461b842bcbfa8ae3d4c3feef05320a3
Shadow <william.shadow@outlook.com>
2021-05-01 21:15:49 +0000

    oops

---
+++ src/rosewheel.js
@@ -0,0 +1,22 @@
+/*
+
+This file holds the prizes for the rose wheel
+Generally lines should only be added by Shadow, but you
+can update the role IDs by simply replacing them.
+
+
+*/
+
+module.exports = [
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+    {name: "Prize", rate: 1},
+]
