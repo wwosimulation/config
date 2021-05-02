e8dc52aae6f2bcc62d5a08a9f19ac0519825b5cd
Jelmer16 <82958337+Jelmer16@users.noreply.github.com>
2021-05-02 17:21:34 +0200

    Update lootbox.js

--- src/lootbox.js
+++ src/lootbox.js
@@ -1,1 +1,20 @@
+/*
+
+This file holds the prizes for the rose wheel
+Generally lines should only be added by Shadow, but you
+can update the role IDs by simply replacing them.
+
+
+*/
+
+module.exports = [
+    {name: "5 coins", rate: 1},
+    {name: "10 coins", rate: 1},
+    {name: "25 coins", rate: 1},
+    {name: "25 coins", rate: 1},
+    {name: "50 coins", rate: 1},
+    {name: "1 rose", rate: 1},
+    {name: "1 rose bouquet", rate: 1},
+    {name: "Icon", rate: 1},
+]
 
