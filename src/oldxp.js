102ef3d815147cc920bb99ae2db4bc3be539f672
thewilloftheshadow <william.shadow@outlook.com>
2021-05-09 23:24:03 +0000

    start new xp

---
+++ src/oldxp.js
@@ -0,0 +1,30 @@
+/*
+
+This file holds all the values for XP given after a game
+
+Format:
+
+"id": number,
+
+The ID is given by Shadow
+
+*/
+
+// XP values for non-teams
+module.exports = {
+  finishGame: 15,
+  winStreak: 25,
+  firstWinOfTheDay: 100,
+  narrator: 150,
+}
+
+// XP Values for each team
+module.exports.team = {
+  couple: 95,
+  werewolf: 75,
+  village: 55,
+  fool: 150,
+  headhunter: 150,
+  solo: 175,
+  tie: 15,
+}
