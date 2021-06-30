3f953f058ee9d822f50c458b5d784f3eb3613d1e
Shadow <thebotoftheshadow@gmail.com>
2021-06-29 23:38:39 -0500

    Create aliases.js

---
+++ src/aliases.js
@@ -0,0 +1,18 @@
+/*
+
+This file holds the aliases for roles
+
+Format:
+"alias": "role name",
+
+All role names need to be lowercase, full name of the role as shown in the roles.js file
+
+Add a line before the } at the end, and make sure you have a comma at the end of the file  
+
+*/
+
+module.exports = {
+    "alias1": "role",
+    "alias2": "role",
+    "alias3": "role 2"
+}
