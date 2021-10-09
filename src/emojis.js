2791b9544b4e29b1467568c41c803cad7da3f42c
Shadow <william.shadow@outlook.com>
2021-10-09 14:34:12 -0500

    New IDs (#37)
    * New game server IDs
    
    * Update emojis.js
    
    * Add corrupted
    
    * Update ids.js

--- src/emojis.js
+++ src/emojis.js
@@ -12,8 +12,8 @@ The ID is given by Shadow
 
 module.exports = {
   coin: `<:coin:606434686931173377>`,
-  rose: `<:rosesingle:807256844191793158>`,
-  gem: `<:gem:833431075309092904>`,
-  lootbox: `<:lootbox:808548473548963861>`,
-  bouquet: `<:rosebouquet:808545517209387008>`,
+  rose: `<:rosesingle:863819859585794108>`,
+  gem: `<:gem:863819867063713823>`,
+  lootbox: `<:lootbox:863819861071495189>`,
+  bouquet: `<:rosebouquet:863819864106991616>`,
 }
