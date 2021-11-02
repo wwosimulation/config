7f4f2a868f10a9942fd44559adba2cd8c4db3cb3
Hackante <85803963+Hackante@users.noreply.github.com>
2021-11-02 22:55:18 +0100

    Rosewheel update (#49)
    * peaceCheck() added in fn.js
    
    * changed an arg so you need less
    
    * changed hacker with prog
    
    * function fix
    
    * changes in peaceCheck
    
    * changed small things in prog desc
    
    * capitalizeFirstLetter
    
    * updated rosewheel items
    
    * updated file description (head)

--- src/rosewheel.js
+++ src/rosewheel.js
@@ -3,19 +3,19 @@
 This file holds the prizes for the rose wheel.
 Name: the name shown to the user
 Rate: higher rate = more common
-ID: One of "coin", "rose", "bouquet", "lootbox", "other"
+Item: location of the data to be updated
 Amount: amount of prize to give
 
 
 */
 
 module.exports = [
-  { name: "100 Coins", rate: 1, item: "coin", amount: 100 },
-  { name: "100 Coins", rate: 1, item: "coin", amount: 100 },
-  { name: "250 Coins", rate: 1, item: "coin", amount: 250 },
-  { name: "250 Coins", rate: 1, item: "coin", amount: 250 },
-  { name: "500 Coins", rate: 1, item: "coin", amount: 500 },
-  { name: "Lootbox", rate: 1, item: "lootbox", amount: 1 },
-  { name: "5 Roses", rate: 1, item: "rose", amount: 5 },
-  { name: "Icon", rate: 1, item: "other", amount: 1 },
+  { name: "100 Coins", rate: 1, item: "coins", amount: 100 },
+  { name: "100 Coins", rate: 1, item: "coins", amount: 100 },
+  { name: "500 Coins", rate: 1, item: "coins", amount: 500 },
+  { name: "250 Coins", rate: 1, item: "coins", amount: 250 },
+  { name: "250 Coins", rate: 1, item: "coins", amount: 250 },
+  { name: "1 Lootbox", rate: 1, item: "inventory.lootbox", amount: 1 },
+  { name: "5 Roses", rate: 1, item: "inventory.rose", amount: 5 },
+  { name: "1 Gem", rate: 1, item: "gems", amount: 1 }, // used to be { name: "Icon", rate: 1, item: "other", amount: 1 }
 ]
