9b9e0a1732027f374c7f95a6b85d47e61e58b5ef
Hackante <kante.hagen@gmail.com>
2021-11-03 22:35:28 +0100

    Icon for Rose wheel prizes

--- src/rosewheel.js
+++ src/rosewheel.js
@@ -5,17 +5,15 @@ Name: the name shown to the user
 Rate: higher rate = more common
 Item: location of the data to be updated
 Amount: amount of prize to give
-
+Icon: Icon of the prize (https://www.wolvesville.com/static/media/ + {icon} + .png)
 
 */
 
 module.exports = [
-  { name: "100 Coins", rate: 1, item: "coins", amount: 100 },
-  { name: "100 Coins", rate: 1, item: "coins", amount: 100 },
-  { name: "500 Coins", rate: 1, item: "coins", amount: 500 },
-  { name: "250 Coins", rate: 1, item: "coins", amount: 250 },
-  { name: "250 Coins", rate: 1, item: "coins", amount: 250 },
-  { name: "1 Lootbox", rate: 1, item: "inventory.lootbox", amount: 1 },
-  { name: "5 Roses", rate: 1, item: "inventory.rose", amount: 5 },
-  { name: "1 Gem", rate: 1, item: "gems", amount: 1 }, // used to be { name: "Icon", rate: 1, item: "other", amount: 1 }
+  { name: "100 Coins", rate: 2, item: "coins", amount: 100, icon: "gold_pile_2.f6035285" },
+  { name: "500 Coins", rate: 1, item: "coins", amount: 500, icon: "gold_pile_5.59503148" },
+  { name: "250 Coins", rate: 2, item: "coins", amount: 250, icon: "gold_pile_3.32199ad2" },
+  { name: "1 Lootbox", rate: 1, item: "inventory.lootbox", amount: 1, icon: "loot_box_icon.c8726777" },
+  { name: "5 Roses", rate: 1, item: "inventory.rose", amount: 5, icon: "rose_inventory_single.eb6af861" },
+  { name: "1 Gem", rate: 1, item: "gems", amount: 1, icon: "gem.439d7650" }, // used to be { name: "Icon", rate: 1, item: "other", amount: 1 }
 ]
