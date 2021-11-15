ee8f1d734dc2d2dc9382e77694f663eff374ec47
Shadow <william.shadow@outlook.com>
2021-11-15 08:18:56 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ module.exports = {
   lootbox: require("./lootbox.js"),
   emojis: require("./emojis.js"),
   soloKillers: ["Serial Killer", "Arsonist", "Bomber", "Corruptor", "Cannibal", "Illusionist", "Bandit", "Alchemist", "Hacker", "Dreamcatcher", "Jack"],
-  gameModes: ["Quick", "Sandbox", "Custom", "Chain Reaction"],
+  gameModes: ["Quick", "Sandbox", "Custom", "Chain Reaction", "Pure Random"],
 }
 
 module.exports.bombPlacements = ["1 2 3", "2 3 4", "5 6 7", "6 7 8", "9 10 11", "10 11 12", "13 14 15", "14 15 16", "1 5 9", "5 9 13", "2 6 10", "6 10 14", "3 7 11", "7 11 15", "4 8 12", "8 12 16", "1 6 11", "2 7 12", "3 6 9", "4 7 10", "5 10 15", "6 11 16", "7 10 13", "8 11 14"]
