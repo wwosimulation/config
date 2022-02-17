ca68d3760acd94aa301515355b926dd57033e3b7
Hackante <85803963+Hackante@users.noreply.github.com>
2022-02-17 18:55:44 +0100

    Gamewarn (#62)
    * ids update
    
    * wrong id
    
    * new id

--- src/ids.js
+++ src/ids.js
@@ -14,6 +14,7 @@ module.exports = {
   corrupted: "892046207214551110",
   immunity: "892046211375333457",
   player: "892046210536468500",
+  member: "606167032425218084",
   staff: "606138123260264488",
   afkstaff: "612750104973017207",
   mini: "892046205117407273", //Narrator Trainee in the game server
@@ -22,10 +23,14 @@ module.exports = {
   narratorsim: "606123619999023114", //Narrator in the main server
   spectator: "892046208061800468",
   jowov: "892046203573915689",
+  gamebanned: "606123628693684245",
   server: {
     log: "693157346771468289",
     sim: "465795320526274561",
     game: "890234659965898813",
+  },
+  channels: {
+    warnLog: "606123769031032863",
   },
   emojis: ["833721024209879050", "834083055987327058"],
 }
