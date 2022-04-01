34dd85d046d1e2a688668ecc9fa3a2fcd6fe083e
Hackante <85803963+Hackante@users.noreply.github.com>
2022-04-01 22:35:54 +0200

    Update shop.js

--- src/shop.js
+++ src/shop.js
@@ -6,7 +6,6 @@ Formats are given above each section.
 */
 
 const emojis = require("./emojis.js")
-const { MessageEmbed } = require("discord.js")
 module.exports = {
   pages: [],
 } // This initalizes the shop
@@ -72,17 +71,18 @@ Format:
 */
 module.exports.icons = [{ name: "Werewolf Berserk Redraw\nCredit to u/Tom_qx", image: "https://media.discordapp.net/attachments/833432936714862682/833433091480485938/berserkdraw.png", id: "wwberserk", price: 10 }]
 
-let embeds = [new MessageEmbed()]
+let embeds = [{fields: []}]
 let addFooter = false
 
 for (let shopitem in module.exports.items) {
   let item = module.exports.items[shopitem]
-  if (embeds[embeds.length - 1].fields.length == 5) embeds.push(new MessageEmbed())
-  embeds[embeds.length - 1].addField(`${item.name} - ${item.price} ${emojis[item.currency]}`, item.description)
+  if (embeds[embeds.length - 1].fields.length == 5) embeds.push({fields: []})
+  embeds[embeds.length - 1].fields.push({name:`${item.name} - ${item.price} ${emojis[item.currency]}`, value: item.description})
 }
 for (let [i, embed] of embeds.entries()) {
-  embed.setFooter({text: `Page ${i + 1}/${embeds.length}`})
-  embed.setTitle("Wolvesville Simulation Store").setColor("#1FFF43")
+  embed.footer = {text: `Page ${i + 1}/${embeds.length}`}
+  embed.title = "Wolvesville Simulation Store"
+  embed.color = 0x1FFF43
 }
 
 module.exports.embeds = embeds
