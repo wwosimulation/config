243c1cf14589df63750014ece84121d0719c31ab
Inno <83353462+inno14@users.noreply.github.com>
2022-04-04 22:55:01 +0530

    Update shop.js (#83)
    * Update shop.js
    
    * Update shop.js
    
    * Update shop.js
    
    * Update src/shop.js
    
    Co-authored-by: Hackante <85803963+Hackante@users.noreply.github.com>
    
    * Update shop.js
    
    * Update src/shop.js
    
    Co-authored-by: Hackante <85803963+Hackante@users.noreply.github.com>
    
    * Update src/shop.js
    
    Co-authored-by: Hackante <85803963+Hackante@users.noreply.github.com>
    
    * Update src/shop.js
    
    Co-authored-by: Hackante <85803963+Hackante@users.noreply.github.com>
    
    * Update shop.js
    
    * Update shop.js
    
    Just told how to edit a pr and i clicked commit changes 😂🤣
    
    Co-authored-by: Hackante <85803963+Hackante@users.noreply.github.com>

--- src/shop.js
+++ src/shop.js
@@ -6,7 +6,6 @@ Formats are given above each section.
 */
 
 const emojis = require("./emojis.js")
-const { MessageEmbed } = require("discord.js")
 module.exports = {
   pages: [],
 } // This initalizes the shop
@@ -35,13 +34,13 @@ module.exports.items = [
   { id: "special", name: "Special Color Role", price: 400, currency: "coin", description: "Get your own color role!" },
   { id: "grey", name: "Grey Color Role", price: 200, currency: "coin", description: "Gain the very rare Grey Color role", role: "606123657210757136" },
   //{ id: "ranked", name: "Ranked Pass", price: 250, currency: "coin", description: "Gain access to play ranked games!", role: "832845602009645116" },
-  //{ id: "emoji", name: "Emoji", price: 450, currency: "coin", description: "Gain access to the `+emoji` and `+emojilist` commands", role: "663389088354664477" },
+  { id: "emoji", name: "Emoji", price: 450, currency: "coin", description: "Gain access to `+write` command. Note: The `+emoji` and `+emojilist` commands are in progress and will be fixed as soon as possible.", role: "663389088354664477" },
   //{ id: "cmi", name: "Custom Maker Item", price: 1000, currency: "coin", description: "You can use this to create your own role list when playing.\nThis does not give you the full set of role list. You need to buy the rest of the roles using coins or roses. To check the list, do `+cmi`!" },
   { id: "channel", name: "Private Channel", price: 100, currency: "rose", description: "Create your own private channel in this server!", role: "627539599862005760" },
   { id: "normal", name: "Normal Item Roulette", price: 10, currency: "coin", description: "Gain access to the item roulette channels to get color roles", role: "606123668162084894", response: "You now have access to the item roulette channels! Use ,ir to use the roulette." },
   { id: "premium", name: "Premium Item Roulette", price: 25, currency: "coin", description: "Gives an higher chance on getting rare roles in item roulette", role: "606123666895274003", response: "You now have an higher chance on rare items in item roulette! Use ,pir to use the elite roulette." },
   { id: "elite", name: "Elite Item Roulette", price: 50, currency: "coin", description: "Gives the highest chance on getting rare roles in item roulette", role: "606123666257870868", response: "You now have the highest chance on rare items in item roulette! Use ,eir to use the elite roulette." },
-  //{ id: "lottery", name: "Lottery ticket", price: 5, currency: "coin", description: "Get a ticket to take part in the lottery!", role: "947629828771831888", response: "You can now enter <#947930500725616700> 🥳"}
+  { id: "auto", name: "Auto Reaction", price: 5, currency: "gem", description: "Get an auto reaction whenever someone pings you!", response: "Ask a staff member to get it at an appropriate place/channel. But before asking keep in mind that Carl-bot must be in the same server with the emoji."}
 ]
 
 /*
@@ -72,17 +71,20 @@ Format:
 */
 module.exports.icons = [{ name: "Werewolf Berserk Redraw\nCredit to u/Tom_qx", image: "https://media.discordapp.net/attachments/833432936714862682/833433091480485938/berserkdraw.png", id: "wwberserk", price: 10 }]
 
-let embeds = [new MessageEmbed()]
+let embeds = [{fields: []}]
 let addFooter = false
 
 for (let shopitem in module.exports.items) {
   let item = module.exports.items[shopitem]
-  if (embeds[embeds.length - 1].fields.length == 5) embeds.push(new MessageEmbed())
-  embeds[embeds.length - 1].addField(`${item.name} - ${item.price} ${emojis[item.currency]}`, item.description)
+  if (embeds[embeds.length - 1].fields.length == 5) embeds.push({fields: []})
+  embeds[embeds.length - 1].fields.push({name:`${item.name} - ${item.price} ${emojis[item.currency]}`, value: item.description})
 }
 for (let [i, embed] of embeds.entries()) {
-  embed.setFooter({text: `Page ${i + 1}/${embeds.length}`})
-  embed.setTitle("Wolvesville Simulation Store").setColor("#1FFF43")
+  embed.footer = {text: `Page ${i + 1}/${embeds.length}`}
+  embed.title = "Wolvesville Simulation Store"
+  embed.color = 0x1FFF43
 }
 
 module.exports.embeds = embeds
+
+
