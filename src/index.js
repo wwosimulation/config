5e0b94990a6f50d4a5f697d0188069d896f47cf0
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-22 21:52:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,3 +43,22 @@ module.exports = {
 }
 
 module.exports.bombPlacements = ["1 2 3", "2 3 4", "5 6 7", "6 7 8", "9 10 11", "10 11 12", "13 14 15", "14 15 16", "1 5 9", "5 9 13", "2 6 10", "6 10 14", "3 7 11", "7 11 15", "4 8 12", "8 12 16", "1 6 11", "2 7 12", "3 6 9", "4 7 10", "5 10 15", "6 11 16", "7 10 13", "8 11 14"]
+
+module.exports.wolfList = {
+  Werewolf: 1,
+  "Junior Werewolf": 2,
+  "Split Wolf": 2,
+  "Kitten Wolf": 2,
+  "Wolf Shaman": 3,
+  "Nightmare Werewolf": 3,
+  "Voodoo Werewolf": 3, 
+  "Wolf Trickster": 3,
+  "Wolf Pacifist": 4, 
+  "Guardian Wolf": 4,
+  "Shadow Wolf": 5,
+  "Werewolf Berserk": 5,
+  "Alpha Werewolf": 6,
+  "Stubborn Werewolf": 6,
+  "Wolf Seer": 7,
+  "Lone Wolf": 8,
+} 
